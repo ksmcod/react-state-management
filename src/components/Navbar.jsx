29d69ab2cd0d9de0ec8289d86768e37023ebc887
Kono Steve <stevekono77@gmail.com>
2023-05-16 20:42:22 +0100

    Using multiple context with useContext hook instead of Context.Provider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,22 @@
+import { useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
 import { ThemeContext } from "../context/ThemeContext"
 
 export default function Navbar() {
-    return (
-        <AuthContext.Consumer >
-            {authContext => {
-                return (
-                <ThemeContext.Consumer >
-                    {themeContext => {
-                        const { isLightTheme,light,dark } = themeContext
-                        const theme = isLightTheme ? light : dark
-                        return (
-                        <div style={{ background:theme.ui, color:theme.syntax }}>
-                            <nav>
-                                <h1>Context App</h1>
-                                <ul>
-                                    <li>Home</li>
-                                    <li>About</li>
-                                    <li>Contact</li>
-                                </ul>
-                            { authContext ? "Logged in!" : "Logged out!"}
-                            </nav>
-                        </div>
-                        )
-                }}
-                </ThemeContext.Consumer>
-                )
-            }}
-        </AuthContext.Consumer>
-    )
+    const { isLightTheme,light,dark } = useContext(ThemeContext)
+    const { isAuth } = useContext(AuthContext)
+    const theme = isLightTheme ? light : dark
+  return (
+    <div style={{ background:theme.ui, color:theme.syntax }}>
+        <nav>
+            <h1>Context App</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        { isAuth ? "Logged in!" : "Logged out!"}
+        </nav>
+    </div>
+  )
 }
